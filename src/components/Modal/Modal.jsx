88a89719d7106d07fd617619fec1e3d92d94371c
elias-291356 <ikucher.002@gmail.com>
2023-11-16 15:46:56 +0400

    change on camelCase inline style of svg

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -45,16 +45,16 @@ const Modal = ({ selectedCar }) => {
                   <path
                     d="M18 6L6 18"
                     stroke="#121417"
-                    stroke-width="1.8"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="1.8"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   />
                   <path
                     d="M6 6L18 18"
                     stroke="#121417"
-                    stroke-width="1.8"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeWidth="1.8"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   />
                 </svg>
               </ButtonModal>
