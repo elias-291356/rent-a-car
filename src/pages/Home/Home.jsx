f55836a74ea7b17d959317d539864d0108f2f363
elias-291356 <ikucher.002@gmail.com>
2023-11-16 15:31:29 +0400

    home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,36 @@
 import React from "react";
+import { BcgWrap, Container, WrapHeadline, WrapText } from "./Home.styled";
 
 const HomePage = () => {
-  return <div>home</div>;
+  return (
+    <>
+      <Container>
+        <WrapHeadline>
+          <h1>Drive Radar</h1>
+          <h2>Welcome to our car search app!</h2>
+        </WrapHeadline>
+        <WrapText>
+          <ul className="list">
+            <li>
+              <p>
+                We provide international search using various parameters, such
+                as car make, rental cost, mileage and others characteristics.
+                Our system will help you quickly find a car, according to your
+                preferences.
+              </p>
+            </li>
+            <li>
+              <p>
+                Save your favorite cars to your favorites list. This is
+                convenient for subsequent quick access and comparison of offers.
+              </p>
+            </li>
+          </ul>
+          <BcgWrap></BcgWrap>
+        </WrapText>
+      </Container>
+    </>
+  );
 };
 
 export default HomePage;
---
+++ src/pages/Home/Home.styled.js
@@ -0,0 +1,45 @@
+import styled from "styled-components";
+export const Container = styled.div`
+  margin-right: auto;
+  margin-left: auto;
+  width: 1183px;
+  padding-bottom: 150px;
+
+
+`;
+export const WrapHeadline = styled.div`
+display:flex;
+align-items: center;
+flex-direction: column;
+color: #121417;
+
+font-family: Manrope,sans-serif;
+font-size: 24px;
+font-style: normal;
+font-weight: 500;
+line-height: 1.5; /* 150% */
+`;
+export const WrapText = styled.div`
+display:flex;
+align-items: center;
+flex-direction: column;
+color: #121417;
+
+font-family: Manrope,sans-serif;
+font-size: 24px;
+font-style: normal;
+font-weight: 500;
+line-height: 1.5; /* 150% */
+`;
+
+export const BcgWrap = styled.div`
+width:100%;
+height: 1000px;
+margin-top: 40px;
+border-radius: 25px;
+background-size: cover;
+  background-repeat: no-repeat; 
+  background-image:  linear-gradient(rgba(209, 59, 59, 0.24),rgba(209, 209, 121, 0.29)),  url(https://img.freepik.com/premium-photo/vector-art-illustration-design-art-vehicles-car-bicycle-motorbike-truck-bus-other_975572-7832.jpg);
+
+`;
+
