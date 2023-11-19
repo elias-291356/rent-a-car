8de62fd5940929b2bb1dd8bb643e6f04d99634d5
elias-291356 <ikucher.002@gmail.com>
2023-11-20 01:00:45 +0400

    Update carSelector.js

--- src/redux/carSelector.js
+++ src/redux/carSelector.js
@@ -1,12 +1,12 @@
 export const selectCars = (state) => state.cars.carItems;
-export const selectAllCarsItem = (state) => state.cars.allCarsItem;
 export const selectLoader = (state) => state.cars.isLoading;
-export const selectCurrentPage = (state) => state.cars.page;
-export const selectCurrentLimit = (state) => state.cars.limit;
 export const selectSubmitForm = (state) => state.cars.isSubmitted;
 export const selectIsOpenModal = (state) => state.cars.isOpenModal;
+export const selectAllCarsItem = (state) => state.cars.allCarsItem;
 export const selectfilteredCars = (state) => state.cars.filteredCars;
 export const selectFavoriteCars = (state) => state.cars.favoriteCars;
+export const selectCurrentLimit = (state) => state.cars.limit;
+export const selectCurrentPage = (state) => state.cars.page;
 export const selectFilteredFavoriteCars = (state) => state.cars.filteredFavoriteCars;
 export const selectIsSubmittedFaforiteForm = (state) => state.cars.isSubmittedFaforiteForm;
 
